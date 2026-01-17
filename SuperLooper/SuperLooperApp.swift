9bc96aa9d504d32cd510dcdce7b26ee3d88bd693
LMI <larrymeyer22@mac.com>
2026-01-17 16:07:17 -0600

    Initial Commit

---
+++ SuperLooper/SuperLooperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperLooperApp.swift
+//  SuperLooper
+//
+//  Created by Larry Meyer on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct SuperLooperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
